d509aca872338df52cc312a53e3b1fdf3cc120e9
Abhishek09-coder <adittya1773@gmail.com>
2025-12-24 16:18:33 +0530

    Configure frontend for production backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,18 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
-});
-
-// Attach token automatically
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
 
 export default api;
